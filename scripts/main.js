09d5593a2cd305004499bd3df1ce1da42c471ba7
Josh <you@example.coexit>
2016-03-24 00:06:04 -0500

    final touches

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,10 @@
 	console.log(data);
 	data.results.forEach(function(val, i, arr) {
 		var outerBox = $('<div class="outerBox"></div>');
-		var imgLink = $('<a></a>', {href: val.url});
-		var imgBox = $('<img/>', {src: val.Images[0].url_75x75});
-		var title = $('<div></div');
-		var titleLink = $('<a></a>', {href: val.url}).html(val.title);
+		var imgLink = $('<a class="img-link"></a>', {href: val.url});
+		var imgBox = $('<img/>', {src: val.Images[0].url_170x135});
+		var title = $('<div class="title"></div');
+		var titleLink = $('<a class="title-link"></a>', {href: val.url}).html(val.title);
 		var seller = $('<a></a>', {href: val.Shop.url}).html(val.Shop.shop_name);
 		var price = $('<span></span>').html('$' + val.price);
 
